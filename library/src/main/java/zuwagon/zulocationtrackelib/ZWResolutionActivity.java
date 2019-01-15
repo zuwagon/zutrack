d3ce5e794213abbd43b6f1686b5048b415a0d526
dilpeshjain <dilpesh.jain@falkonry.com>
2019-01-15 16:31:39 +0530

    removed alert boxes

--- library/src/main/java/zuwagon/zulocationtrackelib/ZWResolutionActivity.java
+++ library/src/main/java/zuwagon/zulocationtrackelib/ZWResolutionActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
+//import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
 import static zuwagon.zulocationtrackelib.Constants.TAG;
@@ -58,25 +58,25 @@ public class ZWResolutionActivity extends Activity {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                         Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                    ContextWrapper cv = new ContextWrapper(this);
-                    cv.setTheme(R.style.Theme_AppCompat_Dialog);
-
-                    new AlertDialog.Builder(cv)
-                            .setIcon(R.drawable.ic_service_notify)
-                            .setMessage(Zuwagon._rationaleTextRes)
-                            .setPositiveButton(Zuwagon._rationalePositiveButtonRes, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-
-                                }
-                            })
-                            .setOnDismissListener(new DialogInterface.OnDismissListener() {
-                                @Override
-                                public void onDismiss(DialogInterface dialog) {
-                                    requestPermissionAccessFineLocation();
-                                }
-                            })
-                            .show();
+//                    ContextWrapper cv = new ContextWrapper(this);
+//                    cv.setTheme(R.style.Theme_AppCompat_Dialog);
+//
+//                    new AlertDialog.Builder(cv)
+//                            .setIcon(R.drawable.ic_service_notify)
+//                            .setMessage(Zuwagon._rationaleTextRes)
+//                            .setPositiveButton(Zuwagon._rationalePositiveButtonRes, new DialogInterface.OnClickListener() {
+//                                @Override
+//                                public void onClick(DialogInterface dialog, int which) {
+//
+//                                }
+//                            })
+//                            .setOnDismissListener(new DialogInterface.OnDismissListener() {
+//                                @Override
+//                                public void onDismiss(DialogInterface dialog) {
+//                                    requestPermissionAccessFineLocation();
+//                                }
+//                            })
+//                            .show();
 
 
                 } else {
